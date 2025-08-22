9e90d52a3be6a8a0c5afb4e9932ccb43885eea05
Utkarshnegi2k5 <utkarshnegi05@gmail.com>
2025-08-22 16:43:03 +0530

    Revert "Merge branch 'master' of https://github.com/Hakentakken/gogreen"
    This reverts commit bf95f7b97f44d08c638366bfe90d1d58e5eb792a, reversing
    changes made to 4d4bb0feb8aa16699c7f803db742224f03a64eee.

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6NV9xeij28SBlvtB48rKbhmC_Ahh4Cqs",
-  authDomain: "thegreengoofficial.firebaseapp.com",
-  projectId: "thegreengoofficial",
-  storageBucket: "thegreengoofficial.appspot.com",
-  messagingSenderId: "243530098047",
-  appId: "1:243530098047:web:3c7733ec2354914829e64a",
-  measurementId: "G-QV6SDZPETX"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  // ...other config
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// ✅ Export auth so you can use it in your components
 export const auth = getAuth(app);
+export const db = getFirestore(app);
